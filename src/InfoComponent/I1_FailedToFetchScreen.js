15bfdc9e83aea67b68e257bc672b5a2bec8a5f64
kahitoz <kshitiz.sinha.025@gmail.com>
2023-09-30 23:09:37 +0530

    Screen Implemented

--- src/InfoComponent/I1_FailedToFetchScreen.js
+++ src/InfoComponent/I1_FailedToFetchScreen.js
@@ -5,7 +5,7 @@ const FailedToFetchScreen = ({Error}) => {
       <div className="w-screen h-screen p-10 bg-background">
         <div className="flex justify-center items-center">
           <img src={niit} alt="niit"></img>
-          <p className="px-5 font-bold">Some error occured - {Error}</p>
+          <p className="px-5 font-bold">Error - {Error}</p>
         </div>
       </div>
     </>
