a34cb8ae6659a822c5cf4318e62dfd17e6878962
kahitoz <kshitiz.sinha.025@gmail.com>
2023-10-25 21:50:36 +0530

    Implemented the image loading feature in the modal

--- src/GlobalComponent/G2_ImageModal.js
+++ src/GlobalComponent/G2_ImageModal.js
@@ -1,4 +1,18 @@
+import {useState} from "react";
+
 const ImageModal = ({ isOpen, isClose }) => {
+  const [selectedImage, setSelectedImage] = useState(null)
+  const handleImageUploadFunc =  (event) =>{
+    const file = event.target.files[0];
+    if(file){
+      const reader = new FileReader();
+      reader.onload = (e) =>  {
+        setSelectedImage(e.target.result);
+      };
+      reader.readAsDataURL(file)
+    }
+  }
+
   return (
     <>
       {isOpen && (
@@ -6,10 +20,26 @@ const ImageModal = ({ isOpen, isClose }) => {
           <div className="bg-background border border-black w-4/12 p-6 rounded-md">
             <div className="flex flex-col items-center">
               <div className="w-20 h-20 bg-red-200 flex rounded-xl cursor-pointer hover:bg-red-400">
-                <p className="text-center hover:text-white">
-                  Tap to select image
-                </p>
+                {selectedImage ? (
+                    <img
+                        src={selectedImage}
+                        alt="Selected Image"
+                        className="w-20 h-20 bg-red-200 rounded-xl cursor-pointer hover:bg-red-400"
+                    />
+                ) : (
+                    <div className="w-20 h-20 bg-red-200 flex rounded-xl cursor-pointer hover:bg-red-400">
+                      <p className="text-center hover:text-white">
+                        Please Select an Image
+                      </p>
+                    </div>
+                )}
               </div>
+              <input
+              type={`file`}
+              id = "imageInput"
+              accept={"image/*"}
+              onChange={handleImageUploadFunc}
+              />
               <p className="font-bold text-center cursor-pointer mt-3 hover:text-red-700">
                 Upload
               </p>
