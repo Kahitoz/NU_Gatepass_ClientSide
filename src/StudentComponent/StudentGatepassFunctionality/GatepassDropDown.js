c8da1bbbd9a7e4c18d399c3d70997f6b8f011021
Kahitoz <kshitiz.sinha.025@gmail.com>
2023-11-13 02:29:30 -0800

    Drop Down now have 5 gatepasses

---
+++ src/StudentComponent/StudentGatepassFunctionality/GatepassDropDown.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import StudentNavbar from "../StudentSkeleton/S1_Navbar";
+import StudentSubNavbar from "../StudentSkeleton/S2_SubNavbar";
+import LocalFixedForm from "./LocalFixed/localfixedform";
+import LocalFlexibleForm from "./LocalFlexible/localflexibleform";
+import OutStationForm from "./Outstation/outstationform";
+import EmergencyForm from "./Emergency/emergencyForm";
+const GatepassDropDown = () => {
+  const [selectedOption, setSelectedOption] = useState("localfixed");
+
+  const handleSelectedOption = (e) => {
+    setSelectedOption(e.target.value);
+  };
+
+  return (
+    <>
+      <div>
+        <StudentNavbar />
+      </div>
+      <div>
+        <StudentSubNavbar />
+      </div>
+
+      <div className="text-center sm:text-left sm:ms-2">
+        <select
+          id="dropdown"
+          name="dropdown"
+          value={selectedOption}
+          onChange={handleSelectedOption}
+          className="p-2 rounded-lg border border-red-100 mt-2 shadow-xl"
+        >
+          <option value="localfixed">Local Fixed</option>
+          <option value="localflexible">Local Flexible</option>
+          <option value="outstation">Outstation</option>
+          <option value="emergency">Emergency</option>
+          <option value="Non Returnable">Non Returnable</option>
+        </select>
+        <div>
+          {selectedOption == "localfixed" && <LocalFixedForm />}
+          {selectedOption == "localflexible" && <LocalFlexibleForm />}
+          {selectedOption == "outstation" && <OutStationForm />}
+          {selectedOption == "emergency" && <EmergencyForm />}
+        </div>
+      </div>
+    </>
+  );
+};
+export default GatepassDropDown;
