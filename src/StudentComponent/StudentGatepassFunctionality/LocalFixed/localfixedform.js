90c7dd082203d75b002bf29f1e5d85054f75c483
Kahitoz <kshitiz.sinha.025@gmail.com>
2023-11-13 22:58:05 +0530

    form implemented

--- src/StudentComponent/StudentGatepassFunctionality/LocalFixed/localfixedform.js
+++ src/StudentComponent/StudentGatepassFunctionality/LocalFixed/localfixedform.js
@@ -1,33 +1,54 @@
-import { useState } from "react";
+  import { useEffect, useState } from "react";
+  import Localfixedlogic from "./localfixedlogic";
+  import { Display_date_time } from "./localfixedlogic";
+  import Cookies from "js-cookie";
 
-const LocalFixedForm = () => {
-  const [date, setDate] = useState("13/11/2023");
-  const [departureTime, setDepartureTime] = useState("12:00:00");
-  const [arrivalTime, setArrivalTime] = useState("5:00:00");
-  return (
-    <div className="shadow-xl rounded-xl p-2 m-2 bg-red-100">
-      <div>
-        <p className="font-bold p-2">Departure and Arrival Date</p>
-        <p className="bg-gray-200 text-gray-700 p-2 rounded-xl ">{date}</p>
+  const LocalFixedForm = () => {
+    const [date, setDate] = useState("");
+    const [departureTime, setDepartureTime] = useState("");
+    const [arrivalTime, setArrivalTime] = useState("");
+
+    const accessToken = Cookies.get("ACCESS_TOKEN");
+
+    useEffect(()=>{
+      const displaydata = async ()=>{
+        try{
+          const data = await Display_date_time(accessToken);
+          console.log("this is data", data)
+          setDate(data.get_date);
+          setDepartureTime(data.startTime);
+          setArrivalTime(data.endTime);
+        }catch(error){
+          console.log(error);
+        }
+      };
+      displaydata();
+    },[])
+
+    return (
+      <div className="shadow-xl rounded-xl p-2 m-2 bg-red-100">
+        <div>
+          <p className="font-bold p-2">Departure and Arrival Date</p>
+          <p className="bg-gray-200 text-gray-700 p-2 rounded-xl ">{date}</p>
+        </div>
+        <div>
+          <p className="font-bold p-2">Departure Date</p>
+          <p className="bg-gray-200 text-gray-700 p-2 rounded-xl ">
+            {departureTime}
+          </p>
+        </div>
+        <div>
+          <p className="font-bold p-2">Arrival Time</p>
+          <p className="bg-gray-200 text-gray-700 p-2 rounded-xl ">
+            {arrivalTime}
+          </p>
+        </div>
+        <div>
+          <button className="bg-text-2 p-3 rounded-lg mt-5 text-white" onClick={()=>Localfixedlogic()}>
+            Apply Gatepass
+          </button>
+        </div>
       </div>
-      <div>
-        <p className="font-bold p-2">Departure Date</p>
-        <p className="bg-gray-200 text-gray-700 p-2 rounded-xl ">
-          {departureTime}
-        </p>
-      </div>
-      <div>
-        <p className="font-bold p-2">Arrival Time</p>
-        <p className="bg-gray-200 text-gray-700 p-2 rounded-xl ">
-          {arrivalTime}
-        </p>
-      </div>
-      <div>
-        <button className="bg-text-2 p-3 rounded-lg mt-5 text-white">
-          Apply Gatepass
-        </button>
-      </div>
-    </div>
-  );
-};
-export default LocalFixedForm;
+    );
+  };
+  export default LocalFixedForm;
