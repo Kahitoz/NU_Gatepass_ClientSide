0651bf0c776b2cef39f3218000e6fa86814c37bd
Kahitoz <kshitiz.sinha.025@gmail.com>
2023-11-19 09:52:44 +0530

    Added logout functionality

--- src/StudentComponent/StudentSkeleton/S1_Navbar.js
+++ src/StudentComponent/StudentSkeleton/S1_Navbar.js
@@ -3,36 +3,57 @@ import Logout from "../icons/icon-logout.png";
 import Strings from "../StudentsScreen/Strings.json";
 import designs from "../StudentStyling/S1_NavbarCSS";
 import { Link } from "react-router-dom";
-import { hover } from "@testing-library/user-event/dist/hover";
 
 const StudentNavbar = () => {
-  
-
+  function clearCookies() {
+    document.cookie =
+      "ACCESS_TOKEN=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  }
   return (
     <div>
       <div>
         <nav className={`${designs.Navbar["d-1"]}`}>
           <div className={`${designs.Navbar["d-2"]}`}>
-            <img src={Niit_logo} alt={`${Strings.navbar_heading_1}`} className={`${designs.Navbar["d-3"]}`} />
+            <img
+              src={Niit_logo}
+              alt={`${Strings.navbar_heading_1}`}
+              className={`${designs.Navbar["d-3"]}`}
+            />
             <h1 className={`${designs.Navbar["d-4"]}`}>
-              {Strings.navbar_heading_1} 
+              {Strings.navbar_heading_1}
             </h1>
           </div>
           <div className={`${designs.Navbar["d-5"]}`}>
             <ul className={`${designs.Navbar["d-6"]}`}>
               <Link to="/student/home">
-              <li className={`${designs.Navbar["d-7"]} hover:cursor-pointer hover:text-Navbar_bg`}>{Strings.list_1}</li>
+                <li
+                  className={`${designs.Navbar["d-7"]} hover:cursor-pointer hover:text-Navbar_bg`}
+                >
+                  {Strings.list_1}
+                </li>
               </Link>
               <Link to="/student/gatepass">
-              <li className={`${designs.Navbar["d-7"]} hover:cursor-pointer hover:text-Navbar_bg`}>{Strings.list_2}</li>  
+                <li
+                  className={`${designs.Navbar["d-7"]} hover:cursor-pointer hover:text-Navbar_bg`}
+                >
+                  {Strings.list_2}
+                </li>
               </Link>
-              <Link to ="/student/newgatepass">
-                <li className={`${designs.Navbar["d-7"]} hover:cursor-pointer hover:text-Navbar_bg`}>NewGatepass</li>
+              <Link to="/student/newgatepass">
+                <li
+                  className={`${designs.Navbar["d-7"]} hover:cursor-pointer hover:text-Navbar_bg`}
+                >
+                  NewGatepass
+                </li>
               </Link>
             </ul>
-            <Link to="/">
-            <img src={Logout} alt={`${Strings["alt-1"]}`} className={`${designs.Navbar["d-8"]} hover: cursor-pointer`} />
-            </Link> 
+            <Link to="/" onClick={() => clearCookies()}>
+              <img
+                src={Logout}
+                alt={`${Strings["alt-1"]}`}
+                className={`${designs.Navbar["d-8"]} hover: cursor-pointer`}
+              />
+            </Link>
           </div>
         </nav>
       </div>
