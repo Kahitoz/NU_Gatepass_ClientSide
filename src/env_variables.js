5e08470379e65dfb050638a6d57aca2a084878cf
Karam <Karam.arora20@st.niituniversity.in>
2023-11-25 12:39:28 +0530

    client_id fix

--- src/env_variables.js
+++ src/env_variables.js
@@ -1,4 +1,4 @@
 const variables={
-    "Client_id":"http://372946592599-u1gj83quodhpdae46ejslj4tto3mn3vn.apps.googleusercontent.com"
+    "Client_id":"372946592599-u1gj83quodhpdae46ejslj4tto3mn3vn.apps.googleusercontent.com"
 }
 export default variables;
