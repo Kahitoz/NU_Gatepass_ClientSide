de5a86a9a1ccd5ea0c373e9aca04080a07c40f4d
Kahitoz <kshitiz.sinha.025@gmail.com>
2023-11-26 23:17:00 +0530

    outstation clock

--- src/StudentComponent/StudentGatepassFunctionality/Outstation/outstationclock.js
+++ src/StudentComponent/StudentGatepassFunctionality/Outstation/outstationclock.js
@@ -1,8 +1,25 @@
-import React, { useState } from "react";
-
+import React, { useState,useEffect } from "react";
+import Cookies from "js-cookie";
+import { GetCurrentTime, GetLowerBoundTime } from "./outstationcheck";
 const CustomClockPicker = ({ onTimeChange }) => {
+  const accessToken=Cookies.get("ACCESS_TOKEN");
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
+  const[startH,setStartH]=useState(0);
+  const[endH,setEndH]=useState(0);
+  const[startM,setStartM]=useState(0);
+  const[endm,setEndM]=useState(0);
+  useEffect(()=>{
+    async function setParameters (accessToken) {
+      const params = await GetLowerBoundTime(accessToken);
+      const lb_time=params.one
+      const Ub_time=params.three;
+      console.log(lb_time.slice(0,3))
+      setStartH(parseInt(lb_time.slice(0,3),10));
+      setEndH(parseInt(Ub_time.slice(0,3),10))
+    }
+     setParameters(accessToken);
+  },[])
 
   const hrsOnChange = (e) => {
     const newHours = e.target.value;
@@ -19,10 +36,10 @@ const CustomClockPicker = ({ onTimeChange }) => {
       return newMinutes;
     });
   };
-
   const hourPicker = (start = 0, end = 23, step = 1) => {
+    
     const hour = [];
-    for (let i = 0; i <= 23; i = i + step) {
+    for (let i = start; i <= end; i = i + step) {
       const formattedValue = i.toString().padStart(2, "0");
       hour.push(formattedValue);
     }
@@ -47,7 +64,7 @@ const CustomClockPicker = ({ onTimeChange }) => {
           onChange={hrsOnChange}
           className="rounded-xl p-1 ms-1"
         >
-          {hourPicker().map((hour) => (
+          {hourPicker(startH,endH).map((hour) => (
             <option key={hour} value={hour}>
               {hour}
             </option>
