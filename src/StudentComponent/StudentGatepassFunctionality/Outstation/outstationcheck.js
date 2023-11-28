5c0b5a5fd5b678b1296f892b3a7438b59d0402aa
kahitoz <kshitiz.sinha.025@gmail.com>
2023-11-28 15:10:17 +0530

    Implemented all the oustation checks

--- src/StudentComponent/StudentGatepassFunctionality/Outstation/outstationcheck.js
+++ src/StudentComponent/StudentGatepassFunctionality/Outstation/outstationcheck.js
@@ -2,6 +2,34 @@ import Strings from "../../StudentsScreen/Strings.json";
 
 const api_head = Strings.api;
 
+export function checkDates(date_1, date_2) {
+  const parsDate_1 = new Date(date_1);
+  const parsDate_2 = new Date(date_2);
+
+  if (parsDate_2 > parsDate_1) {
+    return 1;
+  } else if (parsDate_2 < parsDate_1) {
+    return -1;
+  } else {
+    return 0;
+  }
+}
+
+export async function Get_Date(accessToken) {
+  const route_1 = "/gatepass/v2/student/time";
+  const combined_date_api = api_head.concat(route_1);
+  const fetch_date_time = await fetch(combined_date_api, {
+    headers: {
+      Authorization: accessToken,
+    },
+  });
+
+  const get_date_time = await fetch_date_time.json();
+  const date = get_date_time.date;
+  const time = get_date_time.time;
+  return { date: date, time: time };
+}
+
 export async function GetWarden(access_token) {
   const route_1 = "/gatepass/v2/student/get_warden_details";
   const combined_get_warden_details = api_head.concat(route_1);
@@ -26,7 +54,7 @@ export async function GetCurrentTime(access_token) {
   });
   const get_time = await fetch_time.json();
   const result = get_time.time;
-  const hr = result.slice(0, 3);
+  const hr = result.slice(0, 2);
   const min = result.slice(3, 5);
   return { Hr: hr, Min: min };
 }
@@ -47,37 +75,83 @@ export async function GetLowerBoundTime(accessToken) {
   return { one: lb_time, two: end_time, three: ub_time, four: b_time };
 }
 
-export async function check_selected_time(
-  access_token,
-  departureTime,
-) {
-  const intdHr = parseInt(departureTime.slice(0, 3), 10);
-  const intdM = parseInt(departureTime.slice(3, 5), 10);
-  const current_time = await GetCurrentTime(access_token);
-  const cHr = parseInt(current_time.Hr);
-  const get_result = await GetLowerBoundTime(access_token);
-  const ub_time_hr = parseInt(get_result.three.slice(0, 3), 10);
-  const ub_time_min = parseInt(get_result.three.slice(3, 5), 10);
-  const b_time = parseInt(get_result.four);
-  const start_hr = cHr + b_time;
+export async function check_selected_time(access_token, currentTime) {
+  const timeData = await GetLowerBoundTime(access_token);
+  const start_time = timeData.one;
+  const end_time = timeData.two;
 
-  if (intdHr >= start_hr && intdHr <= ub_time_hr) {
-    if (intdHr === ub_time_hr) {
-      if (intdM <= ub_time_min) {
-        return true;
-      } else {
-        return false;
-      }
+  const getStartHr = start_time.slice(0, 3);
+  const getEndHr = end_time.slice(0, 3);
+
+  const getcurrentHr = currentTime.slice(0, 3);
+
+  const intStartHr = parseInt(getStartHr, 10);
+  const intEndHr = parseInt(getEndHr, 10);
+
+  const intcurrentHr = parseInt(getcurrentHr, 10);
+  if (intcurrentHr > intStartHr && intcurrentHr < intEndHr) {
+    return false;
+  } else {
+    alert("Time selected is not valid");
+    return true;
+  }
+}
+
+export async function check_departure_date(access_token, departureDate) {
+  const get_current_date = await Get_Date(access_token);
+
+  const current_date = get_current_date.date;
+  
+  const val = checkDates(current_date, departureDate)
+
+  if(val === -1){
+    return false;
+  }else{
+    return true;
+  }
+}
+
+export async function CheckBlackList(access_token) {
+  const route = "/gatepass/v2/student/blacklisted/";
+  const combined_api = api_head.concat(route);
+  const response = await fetch(combined_api, {
+    headers: {
+      Authorization: access_token,
+    },
+  });
+  const get_response = await response.json();
+  const get_result = get_response.blacklisted;
+  if (get_result === true) {
+    return true;
+  } else {
+    return get_result;
+  }
+}
+
+export async function CheckOutstation(access_token) {
+  const route = "/gatepass/v2/student/get_gatepass_status_for_outstation";
+  const combined_get_status = api_head.concat(route);
+  const get_result = await fetch(combined_get_status, {
+    headers: {
+      Authorization: access_token,
+    },
+  });
+  const check_result = await get_result.json();
+  if (
+    check_result.recordset[0] !== undefined &&
+    check_result.recordset[0] !== null
+  ) {
+    if (
+      check_result.recordset[0].status === "Approved" ||
+      check_result.recordset[0].status === "CHECKEDOUT" ||
+      check_result.recordset[0].status === "Pending"
+    ) {
+      
+      return true;
     }
+    return true;
   } else {
     return false;
   }
 }
 
-export async function check_selected_date(
-  access_token,
-  departureDate,
-  arrivalDate
-) {}
-
-export async function check_blacklist(access_token) {}
